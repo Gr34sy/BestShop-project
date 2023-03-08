b8959d087e36fb8bc432c79868ee75d0cdc18519
Gr34sy <slupek3@poczta.fm>
2023-03-08 17:26:33 +0100

    sadly i need to start over

--- calc.js
+++ calc.js
@@ -8,14 +8,11 @@ const inputs = document.querySelectorAll("form input");
 
 const summaryTotal = document.querySelectorAll('#total-price');
 
-//input values
-const inputValues = {
-    products: null,
-    orders: null,
-    package: null,
-    accounting: null,
-    terminal: null,
+//calcData
+const calcData = {
+
 }
+
 
 
 //dropdown displaying
@@ -33,41 +30,19 @@ function displayDropdown(){
 
 select.addEventListener('click', displayDropdown);
 
-//summary displaying
-function displaySummary(name){
-    const line = document.querySelector(`li[data-id="${name}"]`);
+//handling input change
+function handleInputChange(){
 
-    if(inputValues[name]){
-        line.style.display = 'block';
-    }else{
-        line.style.display = 'none';
-    }
-    console.log(line);
 }
 
-//calculator functionality
-function handleInputChange(e){
-    if(e.target.type != 'checkbox'){
-        inputValues[e.target.name] = e.target.value ? e.target.value : false;
-    }else{
-        inputValues[e.target.name] = e.target.checked ? true : false;
-    }
-
-    console.log(inputValues);
-    displaySummary(e.target.name);
-}
-
+//setting select value
 function setSelectValue(e){
     const data = e.target.dataset.value;
 
     select.querySelector('#select__text').innerHTML = data.charAt(0).toUpperCase() + data.slice(1);
     select.setAttribute('data-value',data);
-    inputValues.package = select.dataset.value;
-
-    console.log(inputValues);
-    displayDropdown();
-    displaySummary('package');
 }
+
 
 inputs.forEach((input) => input.addEventListener('change', handleInputChange));
 dropdown.addEventListener('click', setSelectValue);
--- dist/calculator.62cd4a25.js
+++ dist/calculator.62cd4a25.js
@@ -5,13 +5,27 @@ const arrowDown = document.querySelector("#icon__arrowDown");
 const arrowUp = document.querySelector("#icon__arrowUp");
 const inputs = document.querySelectorAll("form input");
 const summaryTotal = document.querySelectorAll("#total-price");
-//input values
-const inputValues = {
-    products: null,
-    orders: null,
-    package: null,
-    accounting: null,
-    terminal: null
+//calcData
+const calcData = {
+    products: {
+        amount: 0,
+        price: this.products.amount * 0.5
+    },
+    orders: {
+        amount: 0,
+        price: this.orders.amount * 0.25
+    },
+    package: {
+        price: 0
+    },
+    accounting: {
+        isChecked: false,
+        price: this.accounting.isChecked === true ? 35 : 0
+    },
+    terminal: {
+        isChecked: false,
+        price: this.terminal.isChecked === true ? 5 : 0
+    }
 };
 //dropdown displaying
 function displayDropdown() {
@@ -29,23 +43,34 @@ select.addEventListener("click", displayDropdown);
 //summary displaying
 function displaySummary(name) {
     const line = document.querySelector(`li[data-id="${name}"]`);
-    if (inputValues[name]) line.style.display = "block";
+    const total = document.querySelector("#total-price");
+    if (calcData[name]) line.style.display = "block";
     else line.style.display = "none";
-    console.log(line);
+    switch(name){
+        case "accounting":
+            line.querySelector("span.item__price").innerHTML = "$35";
+            break;
+        case "terminal":
+            line.querySelector("span.item__price").innerHTML = "$5";
+            break;
+    }
 }
 //calculator functionality
 function handleInputChange(e) {
-    if (e.target.type != "checkbox") inputValues[e.target.name] = e.target.value ? e.target.value : false;
-    else inputValues[e.target.name] = e.target.checked ? true : false;
-    console.log(inputValues);
+    if (e.target.type != "checkbox") calcData[e.target.name.amount] = e.target.value ? e.target.value : false;
+    else calcData[e.target.name.isChecked] = e.target.checked ? true : false;
+    console.log(calcData);
     displaySummary(e.target.name);
 }
 function setSelectValue(e) {
     const data = e.target.dataset.value;
     select.querySelector("#select__text").innerHTML = data.charAt(0).toUpperCase() + data.slice(1);
     select.setAttribute("data-value", data);
-    inputValues.package = select.dataset.value;
-    console.log(inputValues);
+    calcData.package.type = select.dataset.value;
+    calcData.package.price = function() {
+        data;
+    };
+    console.log(calcData);
     displayDropdown();
     displaySummary("package");
 }
